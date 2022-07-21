5d7e97a9541a97a458a040d464674f0358b8fae1
Captain4LK <captain4lk@tutanota.com>
2022-07-21 20:57:43 +0200

    Continue HLH_gui

--- headers/HLH_gui.h
+++ headers/HLH_gui.h
@@ -37,8 +37,15 @@ typedef enum
 {
    HLH_GUI_MSG_LAYOUT,
    HLH_GUI_MSG_PAINT,
+   HLH_GUI_MSG_UPDATE,
+   HLH_GUI_MSG_MOUSE_MOVE,
    HLH_GUI_MSG_USR,
 }HLH_gui_msg;
+
+typedef enum
+{
+   HLH_GUI_UPDATE_HOVER = 1,
+}HLH_gui_const;
 
 typedef int (*HLH_gui_msg_handler)(HLH_gui_element *e, HLH_gui_msg, int di, void *dp);
 
@@ -75,6 +82,9 @@ struct HLH_gui_window
    uint32_t *data;
    int width;
    int height;
+   int mouse_x;
+   int mouse_y;
+   HLH_gui_element *hover;
    HLH_gui_rect update_region;
 
 #ifdef HLH_GUI_PLATTFORM_WIN32
@@ -97,9 +107,11 @@ HLH_gui_element *HLH_gui_element_create(size_t bytes, HLH_gui_element *parent, u
 int HLH_gui_element_msg(HLH_gui_element *e, HLH_gui_msg msg, int di, void *dp);
 void HLH_gui_element_move(HLH_gui_element *e, HLH_gui_rect bounds, int always_layout);
 void HLH_gui_element_repaint(HLH_gui_element *e, HLH_gui_rect *region);
+HLH_gui_element *HLH_gui_element_find_by_point(HLH_gui_element *e, int x, int y);
 
 void HLH_gui_draw_block(HLH_gui_painter *p, HLH_gui_rect rect, uint32_t color);
 void HLH_gui_draw_rectangle(HLH_gui_painter *p, HLH_gui_rect rect, uint32_t color_fill, uint32_t color_border);
+void HLH_gui_draw_string(HLH_gui_painter *p, HLH_gui_rect bounds, const char *str, size_t bytes, uint32_t color, int align_center);
 
 HLH_gui_rect HLH_gui_rect_make(int l, int r, int t, int b);
 HLH_gui_rect HLH_gui_rect_intersect(HLH_gui_rect a, HLH_gui_rect b);
@@ -123,6 +135,46 @@ int hlh_gui_window_msg(HLH_gui_element *e, HLH_gui_msg msg, int di, void *dp);
 void hlh_gui_element_paint(HLH_gui_element *e, HLH_gui_painter *p);
 void hlh_gui_update();
 void hlh_gui_window_end_paint(HLH_gui_window *w, HLH_gui_painter *p);
+void hlh_gui_window_input_event(HLH_gui_window *w, HLH_gui_msg msg, int di, void *dp);
+
+#define HLH_GUI_GLYPH_WIDTH (9)
+#define HLH_GUI_GLYPH_HEIGHT (16)
+
+static const uint64_t hlh_gui_font[] = 
+{
+   0x0000000000000000UL, 0x0000000000000000UL, 0xBD8181A5817E0000UL, 0x000000007E818199UL, 0xC3FFFFDBFF7E0000UL, 0x000000007EFFFFE7UL, 0x7F7F7F3600000000UL, 0x00000000081C3E7FUL, 
+   0x7F3E1C0800000000UL, 0x0000000000081C3EUL, 0xE7E73C3C18000000UL, 0x000000003C1818E7UL, 0xFFFF7E3C18000000UL, 0x000000003C18187EUL, 0x3C18000000000000UL, 0x000000000000183CUL, 
+   0xC3E7FFFFFFFFFFFFUL, 0xFFFFFFFFFFFFE7C3UL, 0x42663C0000000000UL, 0x00000000003C6642UL, 0xBD99C3FFFFFFFFFFUL, 0xFFFFFFFFFFC399BDUL, 0x331E4C5870780000UL, 0x000000001E333333UL, 
+   0x3C666666663C0000UL, 0x0000000018187E18UL, 0x0C0C0CFCCCFC0000UL, 0x00000000070F0E0CUL, 0xC6C6C6FEC6FE0000UL, 0x0000000367E7E6C6UL, 0xE73CDB1818000000UL, 0x000000001818DB3CUL, 
+   0x1F7F1F0F07030100UL, 0x000000000103070FUL, 0x7C7F7C7870604000UL, 0x0000000040607078UL, 0x1818187E3C180000UL, 0x0000000000183C7EUL, 0x6666666666660000UL, 0x0000000066660066UL, 
+   0xD8DEDBDBDBFE0000UL, 0x00000000D8D8D8D8UL, 0x6363361C06633E00UL, 0x0000003E63301C36UL, 0x0000000000000000UL, 0x000000007F7F7F7FUL, 0x1818187E3C180000UL, 0x000000007E183C7EUL, 
+   0x1818187E3C180000UL, 0x0000000018181818UL, 0x1818181818180000UL, 0x00000000183C7E18UL, 0x7F30180000000000UL, 0x0000000000001830UL, 0x7F060C0000000000UL, 0x0000000000000C06UL, 
+   0x0303000000000000UL, 0x0000000000007F03UL, 0xFF66240000000000UL, 0x0000000000002466UL, 0x3E1C1C0800000000UL, 0x00000000007F7F3EUL, 0x3E3E7F7F00000000UL, 0x0000000000081C1CUL, 
+   0x0000000000000000UL, 0x0000000000000000UL, 0x18183C3C3C180000UL, 0x0000000018180018UL, 0x0000002466666600UL, 0x0000000000000000UL, 0x36367F3636000000UL, 0x0000000036367F36UL, 
+   0x603E0343633E1818UL, 0x000018183E636160UL, 0x1830634300000000UL, 0x000000006163060CUL, 0x3B6E1C36361C0000UL, 0x000000006E333333UL, 0x000000060C0C0C00UL, 0x0000000000000000UL, 
+   0x0C0C0C0C18300000UL, 0x0000000030180C0CUL, 0x30303030180C0000UL, 0x000000000C183030UL, 0xFF3C660000000000UL, 0x000000000000663CUL, 0x7E18180000000000UL, 0x0000000000001818UL, 
+   0x0000000000000000UL, 0x0000000C18181800UL, 0x7F00000000000000UL, 0x0000000000000000UL, 0x0000000000000000UL, 0x0000000018180000UL, 0x1830604000000000UL, 0x000000000103060CUL, 
+   0xDBDBC3C3663C0000UL, 0x000000003C66C3C3UL, 0x1818181E1C180000UL, 0x000000007E181818UL, 0x0C183060633E0000UL, 0x000000007F630306UL, 0x603C6060633E0000UL, 0x000000003E636060UL, 
+   0x7F33363C38300000UL, 0x0000000078303030UL, 0x603F0303037F0000UL, 0x000000003E636060UL, 0x633F0303061C0000UL, 0x000000003E636363UL, 0x18306060637F0000UL, 0x000000000C0C0C0CUL, 
+   0x633E6363633E0000UL, 0x000000003E636363UL, 0x607E6363633E0000UL, 0x000000001E306060UL, 0x0000181800000000UL, 0x0000000000181800UL, 0x0000181800000000UL, 0x000000000C181800UL, 
+   0x060C183060000000UL, 0x000000006030180CUL, 0x00007E0000000000UL, 0x000000000000007EUL, 0x6030180C06000000UL, 0x00000000060C1830UL, 0x18183063633E0000UL, 0x0000000018180018UL, 
+   0x7B7B63633E000000UL, 0x000000003E033B7BUL, 0x7F6363361C080000UL, 0x0000000063636363UL, 0x663E6666663F0000UL, 0x000000003F666666UL, 0x03030343663C0000UL, 0x000000003C664303UL, 
+   0x66666666361F0000UL, 0x000000001F366666UL, 0x161E1646667F0000UL, 0x000000007F664606UL, 0x161E1646667F0000UL, 0x000000000F060606UL, 0x7B030343663C0000UL, 0x000000005C666363UL, 
+   0x637F636363630000UL, 0x0000000063636363UL, 0x18181818183C0000UL, 0x000000003C181818UL, 0x3030303030780000UL, 0x000000001E333333UL, 0x1E1E366666670000UL, 0x0000000067666636UL, 
+   0x06060606060F0000UL, 0x000000007F664606UL, 0xC3DBFFFFE7C30000UL, 0x00000000C3C3C3C3UL, 0x737B7F6F67630000UL, 0x0000000063636363UL, 0x63636363633E0000UL, 0x000000003E636363UL, 
+   0x063E6666663F0000UL, 0x000000000F060606UL, 0x63636363633E0000UL, 0x000070303E7B6B63UL, 0x363E6666663F0000UL, 0x0000000067666666UL, 0x301C0663633E0000UL, 0x000000003E636360UL, 
+   0x18181899DBFF0000UL, 0x000000003C181818UL, 0x6363636363630000UL, 0x000000003E636363UL, 0xC3C3C3C3C3C30000UL, 0x00000000183C66C3UL, 0xDBC3C3C3C3C30000UL, 0x000000006666FFDBUL, 
+   0x18183C66C3C30000UL, 0x00000000C3C3663CUL, 0x183C66C3C3C30000UL, 0x000000003C181818UL, 0x0C183061C3FF0000UL, 0x00000000FFC38306UL, 0x0C0C0C0C0C3C0000UL, 0x000000003C0C0C0CUL, 
+   0x1C0E070301000000UL, 0x0000000040607038UL, 0x30303030303C0000UL, 0x000000003C303030UL, 0x0000000063361C08UL, 0x0000000000000000UL, 0x0000000000000000UL, 0x0000FF0000000000UL, 
+   0x0000000000180C0CUL, 0x0000000000000000UL, 0x3E301E0000000000UL, 0x000000006E333333UL, 0x66361E0606070000UL, 0x000000003E666666UL, 0x03633E0000000000UL, 0x000000003E630303UL, 
+   0x33363C3030380000UL, 0x000000006E333333UL, 0x7F633E0000000000UL, 0x000000003E630303UL, 0x060F0626361C0000UL, 0x000000000F060606UL, 0x33336E0000000000UL, 0x001E33303E333333UL, 
+   0x666E360606070000UL, 0x0000000067666666UL, 0x18181C0018180000UL, 0x000000003C181818UL, 0x6060700060600000UL, 0x003C666660606060UL, 0x1E36660606070000UL, 0x000000006766361EUL, 
+   0x18181818181C0000UL, 0x000000003C181818UL, 0xDBFF670000000000UL, 0x00000000DBDBDBDBUL, 0x66663B0000000000UL, 0x0000000066666666UL, 0x63633E0000000000UL, 0x000000003E636363UL, 
+   0x66663B0000000000UL, 0x000F06063E666666UL, 0x33336E0000000000UL, 0x007830303E333333UL, 0x666E3B0000000000UL, 0x000000000F060606UL, 0x06633E0000000000UL, 0x000000003E63301CUL, 
+   0x0C0C3F0C0C080000UL, 0x00000000386C0C0CUL, 0x3333330000000000UL, 0x000000006E333333UL, 0xC3C3C30000000000UL, 0x00000000183C66C3UL, 0xC3C3C30000000000UL, 0x0000000066FFDBDBUL, 
+   0x3C66C30000000000UL, 0x00000000C3663C18UL, 0x6363630000000000UL, 0x001F30607E636363UL, 0x18337F0000000000UL, 0x000000007F63060CUL, 0x180E181818700000UL, 0x0000000070181818UL, 
+   0x1800181818180000UL, 0x0000000018181818UL, 0x18701818180E0000UL, 0x000000000E181818UL, 0x000000003B6E0000UL, 0x0000000000000000UL, 0x63361C0800000000UL, 0x00000000007F6363UL,
+};
 
 struct
 {
@@ -246,6 +298,14 @@ void HLH_gui_element_repaint(HLH_gui_element *e, HLH_gui_rect *region)
    }
 }
 
+HLH_gui_element *HLH_gui_element_find_by_point(HLH_gui_element *e, int x, int y)
+{
+   for(int i = 0;i<e->child_count;i++)
+      if(HLH_gui_rect_inside(e->children[i]->clip,x,y))
+         return HLH_gui_element_find_by_point(e->children[i],x,y);
+   return e;
+}
+
 void HLH_gui_draw_block(HLH_gui_painter *p, HLH_gui_rect rect, uint32_t color)
 {
    rect = HLH_gui_rect_intersect(p->clip,rect);
@@ -261,6 +321,48 @@ void HLH_gui_draw_block(HLH_gui_painter *p, HLH_gui_rect rect, uint32_t color)
 
 void HLH_gui_draw_rectangle(HLH_gui_painter *p, HLH_gui_rect rect, uint32_t color_fill, uint32_t color_border)
 {
+   HLH_gui_draw_block(p,HLH_gui_rect_make(rect.l,rect.r,rect.t,rect.t+1),color_border);
+   HLH_gui_draw_block(p,HLH_gui_rect_make(rect.l,rect.l+1,rect.t+1,rect.b-1),color_border);
+   HLH_gui_draw_block(p,HLH_gui_rect_make(rect.r-1,rect.r,rect.t+1,rect.b-1),color_border);
+   HLH_gui_draw_block(p,HLH_gui_rect_make(rect.l,rect.r,rect.b-1,rect.b),color_border);
+   HLH_gui_draw_block(p,HLH_gui_rect_make(rect.l+1,rect.r-1,rect.t+1,rect.b-1),color_fill);
+}
+
+void HLH_gui_draw_string(HLH_gui_painter *p, HLH_gui_rect bounds, const char *str, size_t bytes, uint32_t color, int align_center)
+{
+   HLH_gui_rect old_clip = p->clip;
+   p->clip = HLH_gui_rect_intersect(bounds,old_clip);
+   int x = bounds.l;
+   int y = (bounds.t+bounds.b-HLH_GUI_GLYPH_HEIGHT)/2;
+
+   if(align_center)
+      x+=(bounds.r-bounds.l-bytes*HLH_GUI_GLYPH_WIDTH)/2;
+
+   for(int i = 0;i<bytes;i++)
+   {
+      uint8_t c = str[i];
+      if(c>127)
+         c = '?';
+
+      HLH_gui_rect rect = HLH_gui_rect_intersect(p->clip,HLH_gui_rect_make(x,x+8,y,y+16));
+      const uint8_t *data = (const uint8_t *)hlh_gui_font+c*16;
+
+      for(int iy = rect.t;iy<rect.b;iy++)
+      {
+         uint32_t *dst = &p->data[iy*p->width+rect.l];
+         uint8_t byte = data[iy-y];
+
+         for(int ix = rect.l;ix<rect.r;ix++,dst++)
+         {
+            if(byte&(1<<(ix-x)))
+               *dst = color;
+         }
+      }
+
+      x+=HLH_GUI_GLYPH_WIDTH;
+   }
+
+   p->clip = old_clip;
 }
 
 void hlh_gui_element_paint(HLH_gui_element *e, HLH_gui_painter *p)
@@ -299,6 +401,24 @@ void hlh_gui_update()
          win->update_region = HLH_gui_rect_make(0,0,0,0);
       }
    }
+}
+
+void hlh_gui_window_input_event(HLH_gui_window *w, HLH_gui_msg msg, int di, void *dp)
+{
+   HLH_gui_element *hover = HLH_gui_element_find_by_point(&w->e,w->mouse_x,w->mouse_y);
+
+   if(msg==HLH_GUI_MSG_MOUSE_MOVE)
+      HLH_gui_element_msg(hover,msg,di,dp);
+
+   if(hover!=w->hover)
+   {
+      HLH_gui_element *prev = w->hover;
+      w->hover = hover;
+      HLH_gui_element_msg(prev,HLH_GUI_MSG_UPDATE,HLH_GUI_UPDATE_HOVER,NULL);
+      HLH_gui_element_msg(w->hover,HLH_GUI_MSG_UPDATE,HLH_GUI_UPDATE_HOVER,NULL);
+   }
+
+   hlh_gui_update();
 }
 
 #ifdef HLH_GUI_PLATTFORM_WIN32
@@ -425,6 +545,7 @@ HLH_gui_window *HLH_gui_window_create(const char *title, int width, int height)
 {
    HLH_gui_window *window = (HLH_gui_window *)HLH_gui_element_create(sizeof(*window),NULL,0,hlh_gui_window_msg);
    window->e.window = window;
+   window->hover = &window->e;
    hlh_gui_state.window_count++;
    hlh_gui_state.windows = realloc(hlh_gui_state.windows,sizeof(*hlh_gui_state.windows)*hlh_gui_state.window_count);
    hlh_gui_state.windows[hlh_gui_state.window_count-1] = window;
@@ -483,6 +604,24 @@ int HLH_gui_message_loop()
             HLH_gui_element_msg(&window->e,HLH_GUI_MSG_LAYOUT,0,NULL);
             hlh_gui_update();
          }
+      }
+      else if(event.type==MotionNotify)
+      {
+         HLH_gui_window *window = hlh_gui_find_window(event.xmotion.window);
+         if(window==NULL)
+            continue;
+         window->mouse_x = event.xmotion.x;
+         window->mouse_y = event.xmotion.y;
+         hlh_gui_window_input_event(window,HLH_GUI_MSG_MOUSE_MOVE,0,NULL);
+      }
+      else if(event.type==LeaveNotify)
+      {
+         HLH_gui_window *window = hlh_gui_find_window(event.xcrossing.window);
+         if(window==NULL)
+            continue;
+         window->mouse_x = -1;
+         window->mouse_y = -1;
+         hlh_gui_window_input_event(window,HLH_GUI_MSG_MOUSE_MOVE,0,NULL);
       }
    }
 }
